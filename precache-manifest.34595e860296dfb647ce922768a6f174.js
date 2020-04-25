6bbbf1b3907fd532ada16a3e0366ae0348d2a3ac
Hristophorov Roman <hristophorov.roman@gmail.com>
2020-04-26 00:07:51 +0300

    Initial commit
    Create README.md

---
+++ precache-manifest.34595e860296dfb647ce922768a6f174.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ad05529140267a169f051928137974",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa9fd822f0b204e06d4f",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "93dea048e0cc2fbfb662",
+    "url": "/static/js/2.7b631392.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.7b631392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9fd822f0b204e06d4f",
+    "url": "/static/js/main.60d99c26.chunk.js"
+  },
+  {
+    "revision": "a0235d8c8db3091f926b",
+    "url": "/static/js/runtime-main.eab870a7.js"
+  }
+]);
